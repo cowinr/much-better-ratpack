321e0c5c84d1975b6effa980e85de32eea686e47
Richard Cowin <richard.cowin@hansard.com>
2019-09-29 10:24:56 +0100

    Configure redis host name for GCP. Provide local profile for using redis on localhost.

--- src/main/java/com/example/muchbetter/RedisConfig.java
+++ src/main/java/com/example/muchbetter/RedisConfig.java
@@ -1,7 +1,9 @@
 package com.example.muchbetter;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
 import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.repository.configuration.EnableRedisRepositories;
@@ -10,9 +12,13 @@ import org.springframework.data.redis.repository.configuration.EnableRedisReposi
 @EnableRedisRepositories(basePackages = "com.example.muchbetter")
 public class RedisConfig {
 
+    @Value("${redis.host}")
+    private String host;
+
     @Bean
     JedisConnectionFactory jedisConnectionFactory() {
-        return new JedisConnectionFactory();
+        RedisStandaloneConfiguration config = new RedisStandaloneConfiguration(host, 6379);
+        return new JedisConnectionFactory(config);
     }
 
     @Bean
